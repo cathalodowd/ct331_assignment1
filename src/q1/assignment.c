50328e973236af350a7c53d5cac9d5c756aa99a3
Cathal O' Dowd <cathal.odowd@hotmail.com>
2018-10-25 22:50:23 +0100

    Q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
 
-int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int arg, char* argc[]) {
+	int x = 0;
+	int* y = 0;
+	long z = 0;
+	double * r = 0;
+	char** q = 0;
+
+	
+	printf("int - %d \n", sizeof(x));
+	printf("int* - %d \n", sizeof(y);
+	printf("long - %d \n", sizeof(z));
+	printf("double * - %d \n", sizeof(r));
+	printf("char** - %d \n", sizeof(q));
+
+	
+
 }
